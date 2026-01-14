b437e2c16a13701e14a77e4a028e6f187598af6e
jjacque1 <jacksonjacque.jj@gmail.com>
2026-01-13 22:15:48 -0500

    fun fact server stopped working

--- Readme.md
+++ Readme.md
@@ -1,9 +1,8 @@
-# Reflection Questions 
+# Fun Fact Server down
 
-1. Why was it important to re-format the data from the Useless Facts API before sending it to your own client? What are the benefits of an API providing a clean, minimal response?
+{
+"code": "server_error",
+"message": "dial tcp 10.43.90.45:6379: connect: connection refused",
+"data": null
+}
 
-
-2. In the catch block, why is it better to send a generic error message to the client instead of the actual error object from axios?
-
-
-3. How might you modify this application to get a fact in a different language if the external API supported it (e.g., with a query parameter like ?language=de)?
--- server.js
+++ server.js
@@ -1,30 +1,32 @@
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
+
 const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
+
+// Root route
 app.get("/", (req, res) => {
   res.send("Server is alive");
 });
 
+// Fun fact API route
 app.get("/api/fun-fact", async (req, res) => {
   try {
     const response = await axios.get(
       "https://uselessfacts.jsph.pl/api/v2/facts/random"
     );
 
-    const factText = response.data.text;
-
-    res.json({ fact: factText });
+    res.json({ fact: response.data.text });
   } catch (error) {
-    
     console.error("Error fetching fun fact:", error.message);
-
     res.status(500).json({ error: "Could not fetch fun fact" });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
